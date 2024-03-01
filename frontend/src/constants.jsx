017982c36615dead054db55f2672d25a7d053d52
Kajal Chandail <chandailkajal9@gmail.com>
2024-03-01 22:18:56 +0530

    dev branch created

--- frontend/src/constants.jsx
+++ frontend/src/constants.jsx
@@ -1,2 +1,2 @@
-export const serverUrl = "https://z2nj0ph6-8000.inc1.devtunnels.ms/"
+export const serverUrl = "https://q31k0k2w-8000.inc1.devtunnels.ms/"
 // export const serverUrl = "http://127.0.0.1:8000/"
